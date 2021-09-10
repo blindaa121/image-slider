d678b733d73884ff4b1f8d5ec579b82be1d024ca
Bryan <blindaa121@gmail.com>
2021-09-09 18:26:24 -0700

    Update modal styling

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -5,13 +5,15 @@ import { GrClose } from 'react-icons/gr'
 const Modal = ({ content, setShowModal }) => {
   const { alt, image } = content
   return (
-    <div className='modal-container'>
-      <GrClose className='modal-container-close-btn' onClick={() => setShowModal(false)} />
-      <div className='modal-top-section'>
-        <h1 className='modal-top-header'>{alt}</h1>
-      </div>
-      <div className='modal-bottom-section'>
-        <img className='modal-bottom-section-image' alt={alt} src={`slider-assets/${image}`} />
+    <div className='modal-overlay'>
+      <div className='modal-container'>
+        <GrClose className='modal-container-close-btn' onClick={() => setShowModal(false)} />
+        <div className='modal-top-section'>
+          <h1 className='modal-top-header'>{alt}</h1>
+        </div>
+        <div className='modal-bottom-section'>
+          <img className='modal-bottom-section-image' alt={alt} src={`slider-assets/${image}`} />
+        </div>
       </div>
     </div>
   )

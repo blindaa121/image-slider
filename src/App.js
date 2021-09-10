27530448f20aec0399d5ee1c40444d0d9561fb7f
Bryan <blindaa121@gmail.com>
2021-09-10 10:41:41 -0700

    Update styling

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="app-container">
       <h1 className='app-header'>Smile Bright Dentistry</h1>
-      <span className='app-body'>
+      <span className='app-body-description'>
         Come on down to Smile Bright to get your smile right! We recognize that the right dental technology improves treatment, reduces pain, and speeds recovery. This is why we offer technology such as digital x-rays, digital intra-oral cameras, and free Wi-Fi internet access for our patients. Just another way we show we care about our patients!
         Check out some of our happy patients below :)
       </span>
